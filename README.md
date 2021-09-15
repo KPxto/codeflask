52d37fc91a11267e26fa93eaae4bc1d03149895c
Kpxto <kaioopeixoto@gmail.com>
2021-09-15 14:00:09 -0300

    blueprints

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # codeflask
+
+This is a base code to build web applications.
+
+In this case, we will build an e-commerce delivery site.
---
+++ delivery/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from delivery.ext.site.main import bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(bp)
+    return app
